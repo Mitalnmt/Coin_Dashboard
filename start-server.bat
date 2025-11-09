d5c36405ca5bae46206b3bd9fdd60783e2296bc2
Mital <51521843+Mitalnmt@users.noreply.github.com>
2025-11-10 05:28:30 +0700

    final update 1

--- start-server.bat
+++ start-server.bat
@@ -9,7 +9,8 @@ echo You can access the app at:
 echo   Main page: http://localhost:8000
 echo   Dashboard: http://localhost:8000/frontend/dashboard.html
 echo   Index:     http://localhost:8000/frontend/index.html
+echo   chatbot:     http://localhost:8000/frontend/chatbot.html
 echo.
 echo Press Ctrl+C to stop the server
 echo.
-python -m http.server 8000
+"C:\Users\Administrator\AppData\Local\Programs\Python\Python312\python.exe" -m http.server 8000
